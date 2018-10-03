704723f693945b65008c7b7a0e62a6f8db3eafb8
lab <lab@labs-mac-mini-3.local>
2018-10-03 11:56:18 +0600

    Initial Commit

---
+++ StackViewAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackViewAutoLayout
+//
+//  Created by lab on 10/3/18.
+//  Copyright © 2018 digicon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StackViewAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackViewAutoLayout
+//
+//  Created by lab on 10/3/18.
+//  Copyright © 2018 digicon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
